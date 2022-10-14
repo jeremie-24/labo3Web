79aa51b63cb3526c022aad27a11767887b135981
jeremie-24 <101289840+jeremie-24@users.noreply.github.com>
2022-10-14 12:22:36 -0400

    changement test
    ...

--- models/nouvelle.js
+++ models/nouvelle.js
@@ -1,13 +1,14 @@
 const Model = require('./model');
 module.exports =
     class Nouvelle extends Model {
-        constructor(category, title, text, imageUrl) {
+        constructor(category, title, text, imageUrl, date) {
             super();
             this.Category = category !== undefined ? category : "";
             this.Title = title !== undefined ? title : "";
             this.Text = text !== undefined ? text : "";
             this.ImageUrl = imageUrl !== undefined ? imageUrl : "";
-            this.Date = Date.now();
+            //this.Date = Date.now();
+            this.Date = date !== undefined ? date : "";
 
             this.setKey("Title");
             this.addValidator('Category', 'string');
